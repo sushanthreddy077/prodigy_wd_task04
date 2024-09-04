ec29b69ecc6ae739d0d74ae24e1bc1561b9cefbe
sushanthreddy077 <Sushanthreddyyy@gmail.com>
2024-09-04 11:09:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth Scrolling for Navigation Links
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
